a042608fae98dd5b06ef04d5db62db779a75faba
Tamanna Toma <tamannatoma208542@gmail.com>
2024-03-23 14:53:28 +0600

    login, sign up switching function

--- mysite/authentications/urls.py
+++ mysite/authentications/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login', views.login_user, name='login'),
-    path('signup', views.signup_user(), name='signup'),
+    path('signup', views.signup_user, name='signup'),
 ]
