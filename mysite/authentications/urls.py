fdb92025ae7a38d3676b35aaf7da0147ec76bc1c
Tamanna Toma <tamannatoma208542@gmail.com>
2024-03-23 14:45:19 +0600

    configured signup form

--- mysite/authentications/urls.py
+++ mysite/authentications/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login', views.login_user, name='login'),
+    path('signup', views.signup_user(), name='signup'),
 ]
--- mysite/authentications/views.py
+++ mysite/authentications/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_user(request):
      return render(request, "Authentication.html")
+
+def signup_user(request):
+     return render(request, "Authentication.html")
