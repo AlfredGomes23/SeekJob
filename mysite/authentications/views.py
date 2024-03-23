a5bacc8b0d4f85befbeee93e767dc01856c21355
Tamanna Toma <tamannatoma208542@gmail.com>
2024-03-23 15:02:31 +0600

    login from is functional

--- mysite/authentications/views.py
+++ mysite/authentications/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def login_user(request):
+     if request.method == "POST":
+          username = request.POST["username"]
+          password = request.POST["password"]
+          print(username, password)
+          user = authenticate(request, username=username, password=password)
+          if user is not None:
+               login(request, user)
+               return redirect("home")
+          else:
+               messages.success(request, "There was an error occurred.")
+               return redirect("login")
      return render(request, "Authentication.html")
 
 def signup_user(request):
