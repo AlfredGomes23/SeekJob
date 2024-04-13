8eea15fea1ed67787b1e21ff471146413970a8ba
Tamanna Toma <tamannatoma208542@gmail.com>
2024-04-13 11:28:58 +0600

    created jobs app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mysite',
-    'authentications'
+    'authentications',
+    'jobs',
 ]
 
 MIDDLEWARE = [
