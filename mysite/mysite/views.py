d6221599b7fd31f32970acd9b68a0d55d7199131
AlfredGomes23 <alfredgomes23@gmail.com>
2024-04-25 12:23:16 +0600

    created contact page

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'Home.html',  {'user': request.user})
 
+
+def contact(request):
+    return render(request, 'ContactUS.html')
+
